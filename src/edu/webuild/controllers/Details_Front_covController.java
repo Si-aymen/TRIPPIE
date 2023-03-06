8f128056c6b82a02d1d99cab3bc0a806ab3299d1
aymen <aymen.rahali@esprit.tn>
2023-03-06 12:43:18 +0000

    modify front updated

--- src/edu/webuild/controllers/Details_Front_covController.java
+++ src/edu/webuild/controllers/Details_Front_covController.java
@@ -16,12 +16,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -106,6 +111,26 @@ public class Details_Front_covController implements Initializable {
 
     @FXML
     private void stats(ActionEvent event) {
+    }
+
+    @FXML
+    private void Modify_btu(ActionEvent event) {
+
+        try {
+
+            Parent page1 = FXMLLoader.load(getClass().getResource("/edu/webuild/gui/Front/Modifer_Front_cov.fxml"));
+            Scene scene = new Scene(page1);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(Menu_CoVoiturageController.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+    }
+
+    @FXML
+    private void delete_btu(ActionEvent event) {
     }
 
 }
