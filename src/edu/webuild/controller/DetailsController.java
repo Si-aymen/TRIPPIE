dbbad44d7e63e977a130238530c1ecbab467e34f
guerfala <mohamedtaher.guerfala@esprit.tn>
2023-03-09 23:48:19 +0100

    modifié design intégration

--- src/edu/webuild/controller/DetailsController.java
+++ src/edu/webuild/controller/DetailsController.java
@@ -11,20 +11,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -78,12 +82,20 @@ public class DetailsController implements Initializable {
         }
     }    
 
-    @FXML
-    private void ajouter(ActionEvent event) {
-    }
 
     @FXML
     private void Modify_btu(ActionEvent event) {
+        try {
+
+            Parent page1 = FXMLLoader.load(getClass().getResource("/edu/webuild/gui/modifier_back.fxml"));
+            Scene scene = new Scene(page1);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println(ex);
+
+        }
     }
 
     @FXML
@@ -96,6 +108,10 @@ public class DetailsController implements Initializable {
 
     @FXML
     private void stats(ActionEvent event) {
+    }
+
+    @FXML
+    private void repondre(ActionEvent event) {
     }
     
 }
