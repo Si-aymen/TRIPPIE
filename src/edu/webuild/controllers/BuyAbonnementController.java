a5d3f1b7d3a7fc8b0b586ae34ce3284cf3728a9d
Nour Mtir <mtir.nour@esprit.tn>
2023-03-09 08:44:46 +0100

    buy membership

--- src/edu/webuild/controllers/BuyAbonnementController.java
+++ src/edu/webuild/controllers/BuyAbonnementController.java
@@ -10,7 +10,9 @@ import edu.webuild.services.AbonnementCRUD;
 import edu.webuild.utils.MyConnection;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -19,8 +21,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -38,30 +43,84 @@ public class BuyAbonnementController implements Initializable {
     /**
      * Initializes the controller class.
      */
-   @FXML
-    private ListView<abonnement> listView;
     
-    @FXML
-    private ListView<abonnement> affichageabonnement;
-    @Override
+     @FXML
+    private TextField tfPrix;
+      @FXML
+    private DatePicker tfdateAchat;
+       @FXML
+    private DatePicker tfdateExpiration;
+        @FXML
+    private Button btnAjouter;
     
-    public void initialize(URL url, ResourceBundle rb) {
-        
-        
-        ListView<abonnement> list =(ListView<abonnement>)listView;
-        
-        
-        AbonnementCRUD abonnementCRUD = new AbonnementCRUD();
-        List<abonnement>list2=abonnementCRUD.afficherabonnement();
-        for(int i=0; i<list2.size();i++)
-        {abonnement a =list2.get(i);
-        list.getItems().add(a);
-        }
- 
-    }    
-    @FXML
-private void buyabonnement(ActionEvent event) {
-     
+      //VARIABLES
+        Date dateAchat,dateExpiration;
+    
+@FXML
+private ComboBox<String> cbType;
+
+
+// Initialize the type and price fields
+@Override
+public void initialize(URL url, ResourceBundle rb) {
+    // Add items to the type combo box
+    cbType.getItems().addAll("Gold", "Platinium", "Bronze");
+
+    // Set the price text field to read-only
+    tfPrix.setEditable(false);
+
+    // Set the date picker value to today's date
+    LocalDate today = LocalDate.now();
+    tfdateAchat.setValue(today);
+    tfdateAchat.setDisable(true);
+    tfdateExpiration.setValue(today.plusYears(1));
+    tfdateExpiration.setDisable(true);
 }
+
+@FXML
+private void buyabonnement(ActionEvent event) {
+    // Get the input values from the combo box and date picker
+    String type = cbType.getValue();
+    LocalDate dateExpirationLocal = tfdateExpiration.getValue();
+
+    // Validate the input values
+    if (type == null || dateExpirationLocal == null) {
+        // If any input value is empty, show an error message
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Empty field(s)");
+        alert.setContentText("Please fill all the information.");
+        alert.showAndWait();
+        return;
+    }
+
+    // Create a new abonnement object with the selected type
+    abonnement abonnement = new abonnement(type);
+
+    // Set the dateAchat and dateExpiration attributes
+    LocalDate today = LocalDate.now();
+    abonnement.setDateAchat(java.sql.Date.valueOf(today));
+    abonnement.setDateExpiration(java.sql.Date.valueOf(dateExpirationLocal.plusYears(1)));
+
+    // Insert the new abonnement object into the database
+    AbonnementCRUD abonnementCRUD = new AbonnementCRUD();
+    abonnementCRUD.ajouterabonnement(abonnement);
+
+    // Show a success message
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setTitle("Information Dialog");
+    alert.setHeaderText(null);
+    alert.setContentText("You bought a new memberchip!");
+    alert.show();
+}
+
+// Update the price text field when the user selects a type
+@FXML
+private void updatePrix(ActionEvent event) {
+    String type = cbType.getValue();
+    abonnement abonnement = new abonnement(type);
+    tfPrix.setText(String.valueOf(abonnement.getPrix()));
+}
+
 
 }
